0d1078e2ddf9471d9d96a361324e13fb1a440423
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-09-23 03:01:28 +0700

    setting timer dan berpindah activity

--- app/src/main/java/com/example/myapplication/splashAct.java
+++ app/src/main/java/com/example/myapplication/splashAct.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.concurrent.Delayed;
 
 public class splashAct extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class splashAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // membuat timer dan berpindah activity
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // merubah activity ke activity lain
+                Intent gotogetstarted = new Intent(splashAct.this,getStartedAct.class);
+                startActivity(gotogetstarted);
+                finish();
+            }
+        }, 1000); // 1000ms = 1s
     }
 }
